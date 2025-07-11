ae53d9ab910be3df47ada3d3188a8d3abdb6c88e
sean <seanluse41@gmail.com>
2025-07-11 18:01:33 +0900

    subtitle

--- src/lib/components/TitleAnimation.svelte
+++ src/lib/components/TitleAnimation.svelte
@@ -1,43 +1,56 @@
 <script>
-    import { onMount } from 'svelte';
-    import Vara from 'vara';
-    
-    export let containerId = "vara-container";
-    let containerElement;
-    
-    onMount(() => {
-      const varaInstance = new Vara(
-        `#${containerId}`, 
-        '/PacificoSLO.json',
-        [{
-          text: "March Waters",
-          fontSize: 64,
-          strokeWidth: 1,
-          color: "#1a1a1a",
-          duration: 2000,
-          textAlign: "center"
-        }]
-      );
-      
-      if (containerElement) {
-        containerElement.varaInstance = varaInstance;
-      }
-      return () => {
-        if (varaInstance && typeof varaInstance.destroy === 'function') {
-          varaInstance.destroy();
-        }
-      };
+  import { onMount } from "svelte";
+  import { fly, fade } from "svelte/transition";
+  import { _ } from "svelte-i18n";
+  import Vara from "vara";
+
+  let { containerId = "vara-container" } = $props();
+  let containerElement;
+  let showSubtitle = $state(false);
+
+  let onAnimationFinished = () => {
+    console.log("done");
+    showSubtitle = true;
+  };
+
+  onMount(() => {
+    const varaInstance = new Vara(`#${containerId}`, "/PacificoSLO.json", [
+      {
+        text: "March Waters",
+        fontSize: 64,
+        strokeWidth: 1,
+        color: "#1a1a1a",
+        duration: 2000,
+        textAlign: "center",
+      },
+    ]);
+
+    varaInstance.ready(function () {
+      varaInstance.animationEnd(onAnimationFinished);
     });
-  </script>
-  
-  <div id={containerId} bind:this={containerElement}></div>
-  
-  <style>
-    div {
-      width: 100%;
-      min-height: 80px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+
+    if (containerElement) {
+      containerElement.varaInstance = varaInstance;
     }
-  </style>
+    return () => {
+      if (varaInstance && typeof varaInstance.destroy === "function") {
+        varaInstance.destroy();
+      }
+    };
+  });
+</script>
+
+<div class="w-full min-h-20 flex flex-col justify-center items-center">
+  <div id={containerId} bind:this={containerElement} class="w-full flex justify-center items-center"></div>
+  
+  <div class="mt-4 h-7 flex items-center justify-center">
+    {#if showSubtitle}
+      <p 
+        class="text-lg text-slate-600 text-center italic"
+        in:fly={{ y: 20, duration: 600, opacity: 0 }}
+      >
+        {$_("top.subtitle")}
+      </p>
+    {/if}
+  </div>
+</div>
